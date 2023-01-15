629fa7051cedc9a0cea887e3821d276b7c53de11
Yaroslav-Kapachenia <kapacenaaroslav@gmail.com>
2023-01-15 19:41:33 +0300

    add user.

--- src/com/example/logic/MainLogic.java
+++ src/com/example/logic/MainLogic.java
@@ -12,3 +12,16 @@ public class MainLogic {
         buyerLogic.buy(phone,buyer);
     }
 }
+
+
+
+//public class MainLogic {
+//    public static void main(String[] args) {
+//        BuyerLogic buyerLogic = new BuyerLogic();
+//        Phone phone = new Phone();
+//        phone.setModel("ggdgh444");
+//        Buyer buyer = new Buyer();
+//        buyer.setName("hdbcjhcb");
+//        buyerLogic.buy(phone,buyer);
+//    }
+//}
